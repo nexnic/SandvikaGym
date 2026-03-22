2abf1879f7ea294cfa3a64560c289d664e9b6271
Kent Erik Hole <kent.erik.hole@outlook.com>
2026-03-22 18:03:44 +0100

    building the Product page

---
+++ src/Layout/Product/Product.jsx
@@ -0,0 +1,12 @@
+export default function Product() {
+  return (
+    <div id="sec-products" className="section active">
+      <div className="section-title">Produkter</div>
+      <div className="section-sub">Varelager og prissetting</div>
+      <div className="actions-row">
+        <button className="btn btn-hold">Nytt Produkt</button>
+      </div>
+      <div className="product-grid" id="products-grid"></div>
+    </div>
+  );
+}
